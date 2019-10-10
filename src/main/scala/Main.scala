b5916d65cc13508d3ef37af520865fb8a7ad7375
Edouard Fouché <edouard.fouche@kit.edu>
2019-10-10 17:49:09 -0500

    streamestimator experiment

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -74,6 +74,7 @@ object Main extends LazyLogging {
     case "com.edouardfouche.experiments.PerformanceIndex" => PerformanceIndex.run()
     case "com.edouardfouche.experiments.PerformanceContrast" => PerformanceContrast.run()
     case "com.edouardfouche.experiments.Power" => Power.run()
+    case "com.edouardfouche.experiments.StreamEstimatorPerformance" => StreamEstimatorPerformance.run()
     case _ => throw new Error(s"Unknown experiment $arg")
   }
 
